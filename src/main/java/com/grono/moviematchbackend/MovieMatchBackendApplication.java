2ae86aaf5003b14d7d51d43f1921b2d9ff08718f
amandagrono <59928641+amandagrono@users.noreply.github.com>
2022-04-20 17:31:02 -0400

    deleted commented code

--- src/main/java/com/grono/moviematchbackend/MovieMatchBackendApplication.java
+++ src/main/java/com/grono/moviematchbackend/MovieMatchBackendApplication.java
@@ -10,35 +10,5 @@ public class MovieMatchBackendApplication {
         SpringApplication.run(MovieMatchBackendApplication.class, args);
     }
 
-    /*@Bean
-    CommandLineRunner runner(MovieRepository repository){
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
-
-        return args -> {
-            Movie movie = new Movie(
-                    123L,
-                    "Spider-Man: No Way Home",
-                    "YOOOOOO THREE SPIDERS",
-                    List.of(Genre.Action, Genre.Comedy, Genre.Fantasy, Genre.Adventure),
-                    List.of(StreamingService.DisneyPlus, StreamingService.Showtime),
-                    "https://image.tmdb.org/t/p/w500/nogV4th2P5QWYvQIMiWHj4CFLU9.jpg",
-                    3453.23d,
-                    "Jon Watts",
-                    List.of("Tom Holland", "Andrew Garfield", "Toby Maguire"),
-                    simpleDateFormat.parse("20211216"),
-                    "tt10872600",
-                    "movie",
-                    "PG-13");
-            try{
-                System.out.println("Here");
-                repository.insert(movie);
-            }
-            catch (Exception e){
-                System.out.println("Duplicate Key!");
-            }
-
-        };*/
-
-
 
 }
