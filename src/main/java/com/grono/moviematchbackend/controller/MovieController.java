4210b98bf10cffb37d70ee2840a21712424b9822
amandagrono <59928641+amandagrono@users.noreply.github.com>
2022-04-21 23:07:43 -0400

    omg. the fetching movie things were so much more tedious than I thought they would be. there's definitely a better way I could've written the queries, and maybe I'll handle that later lol

--- src/main/java/com/grono/moviematchbackend/controller/MovieController.java
+++ src/main/java/com/grono/moviematchbackend/controller/MovieController.java
@@ -1,6 +1,7 @@
 package com.grono.moviematchbackend.controller;
 
 import com.grono.moviematchbackend.model.movie.Movie;
+import com.grono.moviematchbackend.model.movie.request.FetchMoviesBody;
 import com.grono.moviematchbackend.model.movie.request.ViewMovieBody;
 import com.grono.moviematchbackend.service.MovieService;
 import lombok.AllArgsConstructor;
@@ -17,11 +18,6 @@ public class MovieController {
 
     private final MovieService movieService;
 
-    @GetMapping
-    public List<Movie> fetchAllMovies(){
-        return movieService.fetchAllMovies();
-    }
-
     @GetMapping(value = "{id}")
     public Movie getMovieById(@PathVariable String id){
         return movieService.getMovieById(Integer.valueOf(id));
@@ -37,5 +33,10 @@ public class MovieController {
     @PostMapping("/view")
     public void viewMovie(@RequestBody ViewMovieBody body){
         movieService.viewMovie(body);
+    }
+    @GetMapping("/fetch")
+    public List<Movie> fetchMovies(@RequestBody FetchMoviesBody body){
+        return movieService.fetch(body);
+        //return null;
     }
 }
--- src/main/java/com/grono/moviematchbackend/repository/GroupRepository.java
+++ src/main/java/com/grono/moviematchbackend/repository/GroupRepository.java
@@ -3,9 +3,11 @@ package com.grono.moviematchbackend.repository;
 import com.grono.moviematchbackend.model.group.Group;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface GroupRepository extends MongoRepository<Group, String> {
 
     Optional<Group> findGroupById(String id);
+    List<Group> findAllById(List<String> id);
 }
